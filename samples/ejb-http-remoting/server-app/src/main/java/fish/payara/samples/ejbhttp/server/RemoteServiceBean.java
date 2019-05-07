437a7fdf1fa42759cc385159508edf5d5e4d2942
Patrik Dudits <patrik.dudits@payara.fish>
2019-05-07 09:16:55 +0200

    PAYARA-3755: Test cases for EJB HTTP Remoting

---
+++ samples/ejb-http-remoting/server-app/src/main/java/fish/payara/samples/ejbhttp/server/RemoteServiceBean.java
@@ -0,0 +1,128 @@
+/*
+ *    DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ *    Copyright (c) [2019] Payara Foundation and/or its affiliates. All rights reserved.
+ *
+ *    The contents of this file are subject to the terms of either the GNU
+ *    General Public License Version 2 only ("GPL") or the Common Development
+ *    and Distribution License("CDDL") (collectively, the "License").  You
+ *    may not use this file except in compliance with the License.  You can
+ *    obtain a copy of the License at
+ *    https://github.com/payara/Payara/blob/master/LICENSE.txt
+ *    See the License for the specific
+ *    language governing permissions and limitations under the License.
+ *
+ *    When distributing the software, include this License Header Notice in each
+ *    file and include the License file at glassfish/legal/LICENSE.txt.
+ *
+ *    GPL Classpath Exception:
+ *    The Payara Foundation designates this particular file as subject to the "Classpath"
+ *    exception as provided by the Payara Foundation in the GPL Version 2 section of the License
+ *    file that accompanied this code.
+ *
+ *    Modifications:
+ *    If applicable, add the following below the License Header, with the fields
+ *    enclosed by brackets [] replaced by your own identifying information:
+ *    "Portions Copyright [year] [name of copyright owner]"
+ *
+ *    Contributor(s):
+ *    If you wish your version of this file to be governed by only the CDDL or
+ *    only the GPL Version 2, indicate your decision by adding "[Contributor]
+ *    elects to include this software in this distribution under the [CDDL or GPL
+ *    Version 2] license."  If you don't indicate a single choice of license, a
+ *    recipient has the option to distribute your version of this file under
+ *    either the CDDL, the GPL Version 2 or to extend the choice of license to
+ *    its licensees as provided above.  However, if you add GPL Version 2 code
+ *    and therefore, elected the GPL Version 2 license, then the option applies
+ *    only if the new code is made subject to such option by the copyright
+ *    holder.
+ */
+
+package fish.payara.samples.ejbhttp.server;
+
+import fish.payara.samples.ejbhttp.api.RemoteService;
+import fish.payara.samples.ejbhttp.api.User;
+
+import javax.ejb.Local;
+import javax.ejb.Remote;
+import javax.ejb.Stateless;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Random;
+import java.util.UUID;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toList;
+
+@Stateless
+@Remote(RemoteService.class)
+public class RemoteServiceBean implements RemoteService {
+    @Override
+    public int simpleOperation(String string, Double boxed) {
+        if (string == null) {
+            return -1;
+        }
+        int result = string.length();
+        if (boxed != null) {
+            return result+boxed.intValue();
+        }
+        return result;
+    }
+
+    @Override
+    public List<String> elementaryListOperation(int size) {
+        if (size == -1) {
+            return null;
+        }
+        return IntStream.range(0, size).mapToObj(i -> UUID.randomUUID().toString()).collect(toList());
+    }
+
+    @Override
+    public Map<String, String> elementaryMapOperation(int size) {
+        if (size == -1) {
+            return null;
+        }
+        Map<String, String> result = new HashMap<>();
+        for (int i=0; i<size; i++) {
+            result.put(UUID.randomUUID().toString(), UUID.randomUUID().toString());
+        }
+        return result;
+    }
+
+    @Override
+    public User createSimpleUser() {
+        User user = new User();
+        user.login = "me";
+        user.createdAt = LocalDate.of(2019, 5, 6);
+        return user;
+    }
+
+    @Override
+    public Optional<User> createOptionalUser(boolean empty) {
+        if (empty) {
+            return Optional.empty();
+        } else {
+            return Optional.of(createSimpleUser());
+        }
+    }
+
+    @Override
+    public User createNestedUser() {
+        return createUser(10);
+    }
+
+    private Random random = new Random();
+
+    private User createUser(int numFriends) {
+        User result = new User();
+        result.login = UUID.randomUUID().toString();
+        result.createdAt = LocalDate.now().minusDays(random.nextInt(3000));
+        if (numFriends > 0) {
+            result.friends = IntStream.range(0, numFriends).mapToObj(i -> createUser(random.nextInt(numFriends >> 1))).collect(toList());
+        }
+        return result;
+    }
+}
